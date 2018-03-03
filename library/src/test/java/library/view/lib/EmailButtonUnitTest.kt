f85e8c6ef616a997301eca7d5e1b5403570223a2
Chris Zhao <xinyue.zhao@immonet.de>
2018-03-03 20:33:57 +0100

    Added tests for layout case

--- library/src/test/java/library/view/lib/EmailButtonUnitTest.kt
+++ library/src/test/java/library/view/lib/EmailButtonUnitTest.kt
@@ -1,9 +1,13 @@
 package library.view.lib
 
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 import io.kotlintest.matchers.shouldBe
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.android.controller.ActivityController
 import org.robolectric.annotation.Config
 import org.hamcrest.CoreMatchers.`is` as matchIs
 import org.mockito.Mockito.`when` as mockWhen
@@ -11,9 +15,12 @@ import org.mockito.Mockito.`when` as mockWhen
 @RunWith(RobolectricTestRunner::class)
 @Config(shadows = [ShadowEmailButton::class])
 class EmailButtonUnitTest {
+    private lateinit var activityCtrl: ActivityController<TestActivity>
+    private val activity: TestActivity
+        get() = activityCtrl.get()
 
     @Test
-    fun shouldEnableOrDisableAccordingToEmailClient() {
+    fun shouldEnableOrDisableAccordingToEmailClient1() {
         val emailButton = EmailButton(context())
         shadowOf(emailButton).shadowFindEmailClient.let { findClient ->
             with(emailButton) {
@@ -24,5 +31,30 @@ class EmailButtonUnitTest {
             }
         }
     }
+
+    @Test
+    fun shouldEnableOrDisableAccordingToEmailClient2() {
+
+        activityCtrl = Robolectric.buildActivity(TestActivity::class.java).setup()
+
+        val emailButton = activity.findViewById<EmailButton>(R.id.emailButton)
+        shadowOf(emailButton).shadowFindEmailClient.let { findClient ->
+            with(emailButton) {
+                isEnabled.shouldBe(findClient)
+                isClickable.shouldBe(findClient)
+                if (findClient) currentTextColor.shouldBe(context().getColor(R.color.selector_email_text_test_color))
+                else currentTextColor.shouldBe(context().getColor(R.color.test_color))
+            }
+        }
+
+
+        activityCtrl.finish()
+    }
 }
 
+class TestActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
